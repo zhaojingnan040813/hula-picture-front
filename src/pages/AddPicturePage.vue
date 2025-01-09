00fd5e4ac031c3e3b384ae7512505cf007dece4d
hula <2026617199@qq.com>
2025-01-10 00:52:54 +0800

    图片上传功能完善，但是标签和分类好像一些小bug

--- src/pages/AddPicturePage.vue
+++ src/pages/AddPicturePage.vue
@@ -1,26 +1,131 @@
 <template>
   <div id="addPicturePage">
+    <h2 style="margin-bottom: 16px">创建图片</h2>
     <PictureUpload :picture="picture" :onSuccess="onSuccess" />
+
+    <a-form v-if="picture" layout="vertical" :model="pictureForm" @finish="handleSubmit">
+<!--   form 表单一定要有name属性   -->
+      <a-form-item label="名称" name="name">
+        <a-input v-model:value="pictureForm.name" placeholder="请输入名称" />
+      </a-form-item>
+      <a-form-item label="简介" name="introduction">
+        <a-textarea
+          v-model:value="pictureForm.introduction"
+          placeholder="请输入简介"
+          :rows="2"
+          autoSize
+          allowClear
+        />
+      </a-form-item>
+
+
+      <a-form-item label="分类" name="category">
+        <a-auto-complete
+          v-model:value="pictureForm.category"
+          :options="categoryOptions"
+          placeholder="请输入分类"
+          allowClear
+        />
+      </a-form-item>
+      <a-form-item label="标签" name="tags">
+        <a-select
+          v-model:value="pictureForm.tags"
+          :options="tagOptions"
+          mode="tags"
+          placeholder="请输入标签"
+          allowClear
+        />
+      </a-form-item>
+
+
+      <a-form-item>
+        <a-button type="primary" html-type="submit" style="width: 100%">创建</a-button>
+      </a-form-item>
+    </a-form>
   </div>
+
 
 </template>
 
 <script setup lang="ts">
 
 import PictureUpload from '@/components/PictureUpload.vue'
-import { ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { editPictureUsingPost } from '@/api/pictureController'
+import { message } from 'ant-design-vue'
 
 const picture = ref<API.PictureVO>()
 const onSuccess = (newPicture: API.PictureVO) => {
   picture.value = newPicture // 更新图片,不是一个属性，而是很多
 }
+const pictureForm = reactive<API.PictureEditRequest>({})
 
+
+
+const router = useRouter()
+
+/**
+ * 提交表单
+ * @param values
+ */
+const handleSubmit = async (values: any) => {
+  const pictureId = picture.value.id // 图片id是不能为空的呀
+  if (!pictureId) {
+    return
+  }
+  const res = await editPictureUsingPost({
+    id: pictureId,
+    ...values,
+  })
+  if (res.data.code === 0 && res.data.data) {
+    message.success('创建成功')
+    // 跳转到图片详情页
+    router.push({
+      path: `/picture/${pictureId}`,
+    })
+  } else {
+    message.error('创建失败，' + res.data.message)
+  }
+}
+
+
+
+const categoryOptions = ref<string[]>([])
+const tagOptions = ref<string[]>([])
+
+// 获取标签和分类选项
+const getTagCategoryOptions = async () => {
+  const res = await listPictureTagCategoryUsingGet()
+  if (res.data.code === 0 && res.data.data) {
+    // 转换成下拉选项组件接受的格式
+    tagOptions.value = (res.data.data.tagList ?? []).map((data: string) => {
+      return {
+        value: data,
+        label: data,
+      }
+    })
+    categoryOptions.value = (res.data.data.categoryList ?? []).map((data: string) => {
+      return {
+        value: data,
+        label: data,
+      }
+    })
+  } else {
+    message.error('加载选项失败，' + res.data.message)
+  }
+}
+
+onMounted(() => {
+  getTagCategoryOptions()
+})
 
 
 </script>
 
 <style scoped>
-
-
-
+#addPicturePage {
+  max-width: 720px;
+  margin: 0 auto;
+}
 </style>
