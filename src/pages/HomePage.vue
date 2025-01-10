a29dc010af4adc6ec6e736c22428349f0dc01e9c
hula <2026617199@qq.com>
2025-01-10 12:03:38 +0800

    图片管理.搜索功能开发完毕

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,122 @@
 <template>
   <div id="homePage">
-    <h1>{{ msg }}</h1>
+    <!-- 搜索框 -->
+    <div class="search-bar">
+      <a-input-search
+        placeholder="从海量图片中搜索"
+        v-model:value="searchParams.searchText"
+        enter-button="搜索"
+        size="large"
+        @search="doSearch"
+      />
+    </div>
+
+
+    <!-- 图片列表 使用 gutter 能让元素之间有一个间距  -->
+    <a-list
+      :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
+      :data-source="dataList"
+      :pagination="pagination"
+      :loading="loading"
+    >
+      <template #renderItem="{ item: picture }">
+        <a-list-item style="padding: 0">
+          <!-- 单张图片 -->
+          <a-card hoverable>
+            <template #cover>
+              <img
+                style="height: 180px; object-fit: cover"
+                :alt="picture.name"
+                :src="picture.url"
+              />
+            </template>
+            <a-card-meta :title="picture.name">
+              <template #description>
+                <a-flex>
+                  <a-tag color="green">
+                    {{ picture.category ?? '默认' }}
+                  </a-tag>
+                  <a-tag v-for="tag in picture.tags" :key="tag">
+                    {{ tag }}
+                  </a-tag>
+                </a-flex>
+              </template>
+            </a-card-meta>
+          </a-card>
+        </a-list-item>
+      </template>
+    </a-list>
   </div>
 </template>
 
 <script setup lang="ts">
-const msg = "欢迎来到编程导航，你将从这里开始项目学习之旅~";
+import { computed, onMounted, reactive, ref } from 'vue'
+import { listPictureVoByPageUsingPost } from '@/api/pictureController'
+import { message } from 'ant-design-vue'
+
+const dataList = ref<API.PictureVO[]>([])
+
+// 数据
+const total = ref(0)
+const loading = ref(true)
+
+// 搜索条件
+const searchParams = reactive<API.PictureQueryRequest>({
+  current: 1,
+  pageSize: 12, // 每页显示的数量,因为12是3和4的倍数，虽然我也是不理解
+  sortField: 'createTime',
+  sortOrder: 'descend',
+})
+
+// 分页参数
+const pagination = computed(() => {
+  return {
+    current: searchParams.current ?? 1,
+    pageSize: searchParams.pageSize ?? 10,
+    total: total.value,
+    // 切换页号时，会修改搜索参数并获取数据
+    onChange: (page: number, pageSize: number) => {
+      searchParams.current = page
+      searchParams.pageSize = pageSize
+      fetchData()
+    },
+    // 显示总条数给移除了，没必要给用户看到
+  }
+})
+
+// 获取数据
+const fetchData = async () => {
+  loading.value = true
+  const res = await listPictureVoByPageUsingPost(searchParams)
+  if (res.data.data) {
+    dataList.value = res.data.data.records ?? []
+    total.value = res.data.data.total ?? 0
+  } else {
+    message.error('获取数据失败，' + res.data.message)
+  }
+  loading.value = false
+}
+
+
+const doSearch = () => {
+  // 重置搜索条件
+  searchParams.current = 1
+  fetchData()
+}
+
+
+// 页面加载时请求一次
+onMounted(() => {
+  fetchData()
+})
 </script>
 
 <style scoped>
-#homePage {
-  display: flex;
+
+
+#homePage .search-bar {
+  max-width: 480px;
+  margin: 0 auto 16px;
 }
+
 </style>
